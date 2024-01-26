b08bfefeec9caae48bb2ca79dcf98cea09ff76da
PRIYANSH-2004 <110007945+PRIYANSH-2004@users.noreply.github.com>
2024-01-26 17:45:37 +0530

    header changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     <h1 className='bg-blue-500'>Music Player</h1>  
-    </>
+    < div>
+     <Header/>
+    </div >
   )
 }
 
