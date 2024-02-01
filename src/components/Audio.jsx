156b298885c479164e255b18808bdc09fada9bfa
PRIYANSH-2004 <110007945+PRIYANSH-2004@users.noreply.github.com>
2024-02-02 00:15:06 +0530

    volume + play/pasue

--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -1,25 +1,114 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
+// import ReactHowler from 'react-howler'
 
 const Audio =()=>{
+    const audioref = useRef(null);
+
+    const play = ()=>{
+        audioref.current.play();
+        console.log(audioref.current);
+    }
+    const pause = ()=>{
+        audioref.current.pause();
+    }
+    const volUp= ()=>{
+        audioref.current.volume +=0.1;
+    }
+    const volDown= ()=>{
+        audioref.current.volume -=0.1;
+    }
+
+    const [vol,setVol ]= useState(30)
+    // as vol changes we set it
+    const volChange = useEffect(()=>{
+            if(audioref)
+            audioref.current.volume=vol/100
+            
+
+    },[vol]);
+
+    const [check , setCheck] = useState(false)
+    // const [remainingTime , setRemainingTime] = useState(audioref.current.duration)
+
+    // let check = true
+    // for play pause
+    const PP = useEffect(()=>{
+        console.log(check);
+        console.log(audioref.current.currentTime);
+        if(check)
+        audioref.current.play();
+        else
+        audioref.current.pause();   
+
+        if((audioref.current.duration -audioref.current.currentTime )==0){
+            console.log("ended");
+            setCheck(true)
+        }
+
+    } ,[check])
+
+
     return(
         <div className="">
 
-        <div class='flex items-center justify-center min-h-screen'>
-            <div class='w-full max-w-lg  mx-auto bg-gray-500 rounded-lg shadow-xl'>
-                <div class='max-w-md mx-auto space-y-6 my-20 px-20'>
-                    <div class="w-full h-full flex flex-col p-4 bg-black/40 rounded-lg">
+        <div className='flex items-center justify-center min-h-screen'>
+            <div className='w-full max-w-lg  mx-auto bg-gray-500 rounded-lg shadow-xl'>
+                <div className='max-w-md mx-auto space-y-6 my-20 px-20'>
+                    <div className="w-full h-full flex flex-col p-4 bg-black/40 rounded-lg">
                         <img src="https://i.scdn.co/image/ab67616d0000b2739e3f258987afca1e41440fb5"/>
-                        <h1 class="font-semibold text-lg text-white mt-3">Alarm</h1>
-                        <p class="font-semibold text-sm text-gray-200">Google</p>
-                        {/* <img class="h-5 w-16 mt-6 filter opacity-60 brightness grayscale invert" src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"/> */}
+                        <h1 className="font-semibold text-lg text-white mt-3">Alarm</h1>
+                        <p className="font-semibold text-sm text-gray-200">Google</p>
+                        {/* <img className="h-5 w-16 mt-6 filter opacity-60 brightness grayscale invert" src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"/> */}
                     </div>
-                <audio controls >
-                    {/* <source src="https://actions.google.com/sounds/v1/alarms/digital_watch_alarm_long.ogg" type="audio/mpeg" /> */}
-                    <source src="https://actions.google.com/sounds/v1/alarms/digital_watch_alarm_long.ogg" type="audio/ogg" />
-                </audio>
+                    <audio autoPlay ref={audioref}  >
+                        <source className='bg-red-500' src="https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg" type="audio/ogg" />
+
+                    </audio>
+
+
+            {/* PLAY */}
+            <button onClick={(e)=>{check=check ? setCheck(false):setCheck(true); }} type="button" className="text-white bg-red-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-red-500 dark:focus:ring-blue-800">
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 21 21">
+                <g fill="none" fillRule="evenodd" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round">
+                    <path d="m4.494 5.535l12-.038a2 2 0 0 1 2 1.845l.006.155V13.5a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2V7.535a2 2 0 0 1 1.994-2"/>
+                    <path fill="currentColor" d="m9.5 12.5l3-2l-3-2z"/>
+                    </g></svg>
+<span className="sr-only">play button</span>
+</button>
+
+
+
+            {/* PLAY */}
+            {/* <button onClick={play} type="button" className="text-white bg-red-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-red-500 dark:focus:ring-blue-800">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 21 21">
+                            <g fill="none" fillRule="evenodd" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round">
+                                <path d="m4.494 5.535l12-.038a2 2 0 0 1 2 1.845l.006.155V13.5a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2V7.535a2 2 0 0 1 1.994-2"/>
+                                <path fill="currentColor" d="m9.5 12.5l3-2l-3-2z"/>
+                                </g></svg>
+            <span className="sr-only">play button</span>
+            </button> */}
+
+
+
+            {/* pause*/}
+            {/* <button onClick={pause} type="button" className="text-white bg-red-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-red-500 dark:focus:ring-blue-800">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 21 21">
+                            <g fill="none" fillRule="evenodd" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round">
+                                <path d="m4.494 5.535l12-.038a2 2 0 0 1 2 1.845l.006.155V13.5a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2V7.535a2 2 0 0 1 1.994-2"/>
+                                <path fill="currentColor" d="m9.5 12.5l3-2l-3-2z"/>
+                                </g></svg>
+            <span className="sr-only">pause button</span>
+            </button> */}
+   
+            {/* <button onClick={volUp}>VOL up</button>
+            <button onClick={volDown}>VOL down</button> */}
+            <input type="range" min="0" max="100" step="3" onChange={(e)=>{setVol(e.target.value) }}/> 
+            {/* since we have to pass value in above, so we used function inside onchnage */}
                 </div>
             </div>
         </div>
+
+        
         </div>
     )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,35 +4,35 @@ function Footer(){
     return (
         
 
-<footer class=" relative inset-x-0 bottom-0 h-16 bg-white dark:bg-gray-900 ">
-    <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 bg-gray-900">
-        <div class="md:flex md:justify-between">
-          <div class="mb-6 md:mb-0">
-              <a href="#" class="flex items-center">
-                  <img src="https://cdn.freelogovectors.net/wp-content/uploads/2023/04/youtube_music_logo-freelogovectors.net_.png" class="h-8 me-3" alt="Music Player Logo" />
-                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Music Player</span>
+<footer className="  bg-white dark:bg-gray-900 ">
+    <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 bg-gray-900">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+              <a href="#" className="flex items-center">
+                  <img src="https://cdn.freelogovectors.net/wp-content/uploads/2023/04/youtube_music_logo-freelogovectors.net_.png" className="h-8 me-3" alt="Music Player Logo" />
+                  <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Music Player</span>
               </a>
           </div>
-          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3 ">
+          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3 ">
               <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources used</h2>
-                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                      <li class="mb-4">
-                          <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
+                  <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources used</h2>
+                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                      <li className="mb-4">
+                          <a href="https://flowbite.com/" className="hover:underline">Flowbite</a>
                       </li>
                       <li>
-                          <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
+                          <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
                       </li>
                   </ul>
               </div>
               <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                      <li class="mb-4">
-                          <a href="#" class="hover:underline ">Instagram</a>
+                  <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                      <li className="mb-4">
+                          <a href="#" className="hover:underline ">Instagram</a>
                       </li>
                       <li>
-                          <a href="#" class="hover:underline">Twitter</a>
+                          <a href="#" className="hover:underline">Twitter</a>
                       </li>
                   </ul>
               </div>
